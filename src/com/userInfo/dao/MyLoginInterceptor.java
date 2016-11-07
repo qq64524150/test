219dc64d43c3700dfa3e5d58b9db3fb0fca04b45
Administrator <Administrator@PC-201610212230>
2016-11-07 14:51:15 +0800

    修改

--- src/com/userInfo/dao/MyLoginInterceptor.java
+++ src/com/userInfo/dao/MyLoginInterceptor.java
@@ -7,7 +7,7 @@ import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
 
 public class MyLoginInterceptor extends MethodFilterInterceptor{
-
+	///
 	@Override
 	protected String doIntercept(ActionInvocation arg0) throws Exception {
 		ActionContext ctx=arg0.getInvocationContext();
